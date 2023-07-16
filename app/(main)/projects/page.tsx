e7d78fa45fad5192febe3839a593986aebde11e4
Gu Bicheng <e0374027@u.nus.edu>
2023-07-16 15:35:39 +0800

    fixed project

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -5,7 +5,7 @@ import { Container } from '~/components/ui/Container'
 
 const title = 'My Projects'
 const description =
-  '多年来，我一直在做各种各样的小项目，这里就是我筛选出来我觉得还不错的项目合集，也是我在技术领域中尝试和探索的最好见证。'
+  "I've been working on a variety of small projects, and this is a collection of the ones I think are pretty good. It's the best testament to my attempts and explorations in the tech field."
 export const metadata = {
   title,
   description,
@@ -25,12 +25,10 @@ export default function ProjectsPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          My Past Project Explorations.
+          My past project adventures
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          多年来，我一直在做各种各样的小项目，有<b>开源</b>的，有<b>实验</b>
-          的，也有 <b>just for fun </b>
-          的，下面就是我筛选出来我觉得还不错的项目合集，也是我在技术领域中尝试和探索的最好见证。
+          I've been working on a variety of small projects, and this is a collection of the ones I think are pretty good. It's the best testament to my attempts and explorations in the tech field.
         </p>
       </header>
       <div className="mt-16 sm:mt-20">
