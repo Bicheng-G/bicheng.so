ef304b8481403093c4133c347808f5a7504918a5
Gu Bicheng <e0374027@u.nus.edu>
2023-07-18 16:49:21 +0800

    added vercel analytics

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react'
 
 import { BlogPosts } from '~/app/(main)/blog/BlogPosts'
@@ -30,6 +31,7 @@ export default function BlogHomePage() {
           </aside>
         </div>
       </Container>
+      <Analytics />
     </>
   )
 }
