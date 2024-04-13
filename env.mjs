d1d533a0c28fbbe9ac4ba60242c019188d8bb020
Alfie <bicheng.gu@yahoo.com>
2024-04-14 02:06:50 +0800

    fix vercel deployment

--- env.mjs
+++ env.mjs
@@ -7,7 +7,7 @@ const server = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']),
   DATABASE_URL: z.string().min(1),
   RESEND_API_KEY: z.string().min(1),
-  VERCEL_ENV: z.enum(['development', 'preview', 'production']),
+  VERCEL_ENV: z.enum(['development', 'preview', 'production']).default('development'),
   UPSTASH_REDIS_REST_URL: z.string().min(1),
   UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
   LINK_PREVIEW_API_BASE_URL: z.string().optional(),
@@ -18,6 +18,9 @@ const client = z.object({
   NEXT_PUBLIC_SANITY_PROJECT_ID: z.string().min(1),
   NEXT_PUBLIC_SANITY_DATASET: z.string().min(1),
   NEXT_PUBLIC_SANITY_USE_CDN: z.boolean(),
+  NEXT_PUBLIC_SITE_URL: z.string().min(1),
+  NEXT_PUBLIC_SITE_EMAIL_FROM: z.string().min(1),
+  NEXT_PUBLIC_SITE_LINK_PREVIEW_ENABLED: z.boolean().optional().default(false),
 })
 
 /**
@@ -36,6 +39,9 @@ const processEnv = {
   NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
   NEXT_PUBLIC_SANITY_USE_CDN: process.env.NEXT_PUBLIC_SANITY_USE_CDN == 'true',
+  NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  NEXT_PUBLIC_SITE_EMAIL_FROM: process.env.NEXT_PUBLIC_SITE_EMAIL_FROM,
+  NEXT_PUBLIC_SITE_LINK_PREVIEW_ENABLED: process.env.NEXT_PUBLIC_SITE_LINK_PREVIEW_ENABLED == 'true',
   LINK_PREVIEW_API_BASE_URL: process.env.LINK_PREVIEW_API_BASE_URL,
   SITE_NOTIFICATION_EMAIL_TO: process.env.SITE_NOTIFICATION_EMAIL_TO,
 }
