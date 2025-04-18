2f97c40e00d651e1b5bb17047eb09e029414aa1b
Alfie <bicheng.gu@yahoo.com>
2025-04-18 16:35:00 +0800

    Updated seo description

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,7 +1,7 @@
 export const seo = {
   title: 'Bicheng Gu | B for Benevolent',
   description:
-    "I'm Bicheng, a Senior Product Analyst passionate about data-driven product management. Join me as I explore self-actualization, metacognition, and the unique lessons of parenting.",
+    "I'm Bicheng, a Product Manager passionate about self-actualization, metacognition, and parenting.",
   url: new URL(
     process.env.NODE_ENV === 'production'
       ? 'https://bicheng.me'
